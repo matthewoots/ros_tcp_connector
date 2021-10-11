eefd87f96a85d846793e685ea0202de71779d5b0
matthewoots <matthewoots@gmail.com>
2021-10-11 22:37:10 +0800

    modified:   ros_unity/launch/tcp_server.launch 	new file:   ros_unity/msg/empty.msg 	modified:   ros_unity/scripts/tcp_server_endpoint.py

--- ros_unity/scripts/tcp_server_endpoint.py
+++ ros_unity/scripts/tcp_server_endpoint.py
@@ -3,8 +3,21 @@
 import rospy
 
 from ros_tcp_endpoint import TcpServer, RosPublisher, RosSubscriber, RosService
+
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import TwistStamped
+
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
+from sensor_msgs.msg import BatteryState
+from sensor_msgs.msg import NavSatFix
+
+from std_msgs.msg import Bool
+from std_msgs.msg import Byte
+
+from mavros_msgs.msg import State
+from mavros_msgs.msg import ActuatorControl
+
 
 def main():
     ros_node_name = rospy.get_param("/TCP_NODE_NAME", 'TCPServer')
@@ -12,10 +25,18 @@ def main():
 
     # Create ROS communication objects dictionary for routing messages
     tcp_server.source_destination_dict = {
-        #'local_pose_enu': RosSubscriber('mavros/local_position/pose', PoseStamped, tcp_server),
-        'gndcamera1': RosPublisher('gndcamera1', Image, tcp_server),	
-        'gndcamera2': RosPublisher('gndcamera2', Image, tcp_server),
-	'aircamera1': RosPublisher('aircamera1', Image, tcp_server)
+        'local_pos': RosSubscriber('mavros/local_position/pose', PoseStamped, tcp_server),
+        'local_vel': RosSubscriber('mavros/local_position/velocity_local', TwistStamped, tcp_server),
+	'mavros_state': RosSubscriber('mavros/state', State, tcp_server),
+	'batt': RosSubscriber('mavros/battery', BatteryState, tcp_server),
+	#'global_pos': RosSubscriber('mavros/global_position/local', NavSatFix, tcp_server),
+
+        #'Image_Left': RosPublisher('tiscamera_ros/fisheye_left/image_rect_raw', Image, tcp_server),	
+        #'Image_Right': RosPublisher('tiscamera_ros/fisheye_right/image_rect_raw', Image, tcp_server),
+        #'CameraInfo_Left': RosPublisher('tiscamera_ros/fisheye_left/camera_info', CameraInfo, tcp_server),
+        #'CameraInfo_Right': RosPublisher('tiscamera_ros/fisheye_right/camera_info', CameraInfo, tcp_server),
+        #'Mapping': RosPublisher('engage_cmd', Bool, tcp_server),
+	'command': RosPublisher('user', Byte, tcp_server)
     }
 
     # Start the Server Endpoint
